fe1d13826acbe08c6af5259cfda72faac54964a5
Rohan <rohan@zool.in>
2019-05-14 16:36:35 +0530

    some bug fixes

--- commands/full_microservice.js
+++ commands/full_microservice.js
@@ -6,35 +6,42 @@ const packageFile = require('../data/package');
 const lint = require('../data/eslint');
 
 const genMs = (data, callback) => {
-    nodeCmd.get(`${command} ${data.cloud}-${data.platform} --path ${filePath}`, (err, data, stdErr) => {
-        if (err || stdErr) {
-            let error = err ? err : stdErr;
-            return callback(error, null)
+    nodeCmd.get("npm install -g serverless", (err, data, stdErr) => {
+        if (err) {
+            return callback(`Something went wrong ${err}`)
         } else {
-            fs.writeFile(`${filePath}/package.json`, packageFile.package(), (err) => {
-                if (err) {
-                    return callback(`error creating package.json file, ${err}`)
+            nodeCmd.get(`${command} ${data.cloud}-${data.platform} --path ${filePath}`, (err, data, stdErr) => {
+                if (err || stdErr) {
+                    let error = err ? err : stdErr;
+                    return callback(error, null)
                 } else {
-                    fs.writeFile(`${filePath}/.eslint.json`, lint.eslint(), (err) => {
+                    fs.writeFile(`${filePath}/package.json`, packageFile.package(), (err) => {
                         if (err) {
-                            return callback(`error creating lint file, ${err}`)
-                        }
-                        fs.mkdir(`${filePath}/resources`);
-                        fs.mkdir(`${filePath}/services`);
-                        nodeCmd.get(`cd ${filePath} && npm install`, (err, data, stdErr) => {
-                            if (err) {
-                                // let error = err ? err : stdErr;
-                                return callback(error, null);
-                            } else {
-                                return callback(null, true)
-                            }
-                        })
+                            return callback(`error creating package.json file, ${err}`)
+                        } else {
+                            fs.writeFile(`${filePath}/.eslint.json`, lint.eslint(), (err) => {
+                                if (err) {
+                                    return callback(`error creating lint file, ${err}`)
+                                }
+                                fs.mkdir(`${filePath}/resources`);
+                                fs.mkdir(`${filePath}/services`);
+                                nodeCmd.get(`cd ${filePath} && npm install`, (err, data, stdErr) => {
+                                    if (err) {
+                                        // let error = err ? err : stdErr;
+                                        return callback(error, null);
+                                    } else {
+                                        return callback(null, true)
+                                    }
+                                })
 
+                            })
+                        }
                     })
                 }
-            })
+            });
         }
-    });
+    })
+
 }
 
 module.exports = genMs;
